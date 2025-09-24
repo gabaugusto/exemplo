4051ef234faf81b7e82f163afde2e93304291572
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2025-09-24 17:24:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Olá, Mundo</h1>
+
+      <div className="card">
+        Som de Chuva
+      </div>
+
+      <div className="card">
+        Som de Vento
+      </div>
+
+      <div className="card">
+        Som de Fogueira
+      </div>
+    </>
+  )
+}
+
+export default App
