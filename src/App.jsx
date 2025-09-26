042e10cc4ba22bc99bbe2af86f4e1f74158b839f
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2025-09-26 17:38:18 -0300

    Passo 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,43 @@ import { IoRainy, IoFlame, IoLeaf } from "react-icons/io5";
 
 export default function App() {
   const playSound = (soundType) => {
+
     const audio = new Audio();
+
     switch(soundType) {
       case 'rain':
         // Using a nature sounds API or local file
         console.log('Tocar som de chuva...');
+        audio.src = '/ambient-noise.mp3';
+        audio.play();
         break;
       case 'fire':
         console.log('Tocar som de fogueira...');
+        audio.src = '/ambient-noise-2.mp3';
+        audio.play();
         break;
       case 'forest':
         console.log('Tocar som de floresta...');
+        audio.src = '/ambient-noise.mp3';
+        audio.play();
         break;
     }
   };
 
+
+  function tocarSom(soundType){
+    console.log('Tocando som de ' + soundType);
+  }
+
+
+  //var exemplo = function tocarSom() { console.log('Tocando som...') } // Function expression atribuida a variavel
+  //var exemplo = () => {console.log("Tocando um som...")} // Arrow function atribuida a variavel
+  //exemplo()
+
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Sounds</h1>
+        <h1>Sounds</h1>''
         <p>Relaxe e concentre-se com sons da natureza</p>
       </header>
 
